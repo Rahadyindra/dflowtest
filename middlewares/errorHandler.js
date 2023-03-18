82473fbac3a403aa998c5be958625b2ae0f22ab8
Rahadyindra <Rahadyindra16juni@gmail.com>
2023-03-18 22:57:50 +0700

    gitignore

--- middlewares/errorHandler.js
+++
@@ -1,7 +0,0 @@
-function errorHandler(err, req, res, next) {
-  if (err.name === "invalid") {
-    res.status(401).json({ message: "Invalid Input" });
-  } else {
-    res.status(500).json({ message: "Something is wrong with the server" });
-  }
-}
