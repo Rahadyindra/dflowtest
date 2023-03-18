7ab2f4c40c0815a4550b502c286ecbfe3db7a8b8
Rahadyindra <Rahadyindra16juni@gmail.com>
2023-03-18 22:56:02 +0700

    feat: -added endpoint to delete and update

---
+++ server/middlewares/errorHandler.js
@@ -0,0 +1,10 @@
+function errorHandler(err, req, res, next) {
+  
+  if (err.name === "invalid") {
+    res.status(401).json({ message: "Invalid Input" });
+  } else {
+    res.status(500).json({ message: "Something is wrong with the server" });
+  }
+}
+
+module.exports = errorHandler;
